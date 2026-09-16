ab8c1d19f733d231d8ed600c8ab7eee7b50f43db
Atri Thakar <atri.thakar@esparkbizmail.com>
2026-09-16 13:54:15 +0530

    Restructured how server containers are created

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const http = require("http");
 
+const server_num = process.env.SERVER_NO
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("<h1>Hello World From Server 2!</h1>");
+  res.end(`<h1>Hello World From Server ${server_num}!</h1>`);
 });
 
 server.listen(3000, () => {
