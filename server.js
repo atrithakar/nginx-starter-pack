78150d6242ac71fcea52aa6a82b2acbd8d0b045c
Atri Thakar <atri.thakar@esparkbizmail.com>
2026-09-18 18:58:32 +0530

    Experimental

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const http = require("http");
 
-const server_num = process.env.SERVER_NO
+const server_num = process.env.SERVER_NO;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end(`<h1>Hello World From Server ${server_num}!</h1>`);
+  // Check if the request is for the root route '/'
+  if (req.url === "/") {
+    // Delay for 30 seconds (30,000 milliseconds)
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(`<h1>Hello World From Server ${server_num}!</h1><p>Host header received: ${req.headers.host}</p>`);
+  } else {
+    // Handle other routes or return a 404
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(`Request URL Received: ${req.url}`);
+  }
 });
 
 server.listen(3000, () => {
